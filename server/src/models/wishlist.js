795c3ad2306561accf2f3acc9bae7a0d29b11b49
sliance <sliance2402@gmail.com>
2022-10-07 14:21:53 +0700

    Fix brand to wishlist in wishlist.js

--- server/src/models/wishlist.js
+++ server/src/models/wishlist.js
@@ -2,27 +2,24 @@
 import bcrypt from "bcrypt";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-	class Brand extends Model {
-		/**
-		 * Helper method for defining associations.
-		 * This method is not a part of Sequelize lifecycle.
-		 * The `models/index` file will call this method automatically.
-		 */
-		static associate(models) {
-			// define association here
-		}
-	}
-	Wishlist.init(
-		{
-			user_id: DataTypes.INTEGER,
-			product_id: DataTypes.INTEGER,
-		},
-		{
-			sequelize,
-			modelName: "Wishlist",
-		},
-	);
+    class Wishlist extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    Wishlist.init({
+        user_id: DataTypes.INTEGER,
+        product_id: DataTypes.INTEGER,
+    }, {
+        sequelize,
+        modelName: "Wishlist",
+    }, );
 
-	
-	return Wishlist;
+
+    return Wishlist;
 };
