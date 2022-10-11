03ba5986368b078cc99c3133a7e271bbf71551e7
sliance <sliance2402@gmail.com>
2022-10-12 00:04:10 +0700

    rename field subcategories_name to subcategory_name

--- server/src/models/subcategories.js
+++ server/src/models/subcategories.js
@@ -2,27 +2,24 @@
 
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-	class Subcategories extends Model {
-		/**
-		 * Helper method for defining associations.
-		 * This method is not a part of Sequelize lifecycle.
-		 * The `models/index` file will call this method automatically.
-		 */
-		static associate(models) {
-			// define association here
-		}
-	}
-	Subcategories.init(
-		{
-			categories_id: DataTypes.INTEGER,
-			subcategories_name: DataTypes.STRING,
-		},
-		{
-			sequelize,
-			modelName: "Subcategories",
-		},
-	);
+    class Subcategories extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    Subcategories.init({
+        categories_id: DataTypes.INTEGER,
+        subcategory_name: DataTypes.STRING,
+    }, {
+        sequelize,
+        modelName: "Subcategories",
+    }, );
 
-	
-	return Subcategories;
+
+    return Subcategories;
 };
