9298d1fa6bec185c210b27998fe879975b4a7009
sliance <sliance2402@gmail.com>
2022-10-11 21:36:24 +0700

    fix url, add route CRUD category

--- server/src/route/category.js
+++ server/src/route/category.js
@@ -1,9 +1,9 @@
 import express from "express";
 import CategoryController from "../controller/CategoryController";
 const route = express.Router();
-route.get("/category", CategoryController.category);
-route.post("/storeCategory", CategoryController.storeCategory);
-route.post("/updateCategory", CategoryController.updateCategory);
-route.delete("/deleteCategory", CategoryController.deleteCategory);
-route.get("/category/:categoryId", CategoryController.categoryById);
+route.get("/subcategory", CategoryController.category);
+route.post("/storeSubCategory", CategoryController.storeCategory);
+route.put("/updateSubCategory/:id", CategoryController.updateCategory);
+route.delete("/deleteSubCategory/:id", CategoryController.deleteCategory);
+route.get("/subcategory/:id", CategoryController.categoryById);
 module.exports = route;
