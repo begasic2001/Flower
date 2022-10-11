8d215ac7ba6961ff1107e2be2530a2b8a962eb36
sliance <sliance2402@gmail.com>
2022-10-11 21:49:45 +0700

    add validate's category

--- server/src/config/validatation.js
+++ server/src/config/validatation.js
@@ -1,23 +1,30 @@
 import Joi from "joi";
 
 const userValidate = (data) => {
-	const userSchema = Joi.object({
-		email: Joi.string()
-			.email({
-				minDomainSegments: 2,
-				tlds: { allow: ["com", "net"] },
-			})
-			.lowercase()
-			.required(),
-		password: Joi.string()
-			.pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
-			.required(),
-		
-	});
+    const userSchema = Joi.object({
+        email: Joi.string()
+            .email({
+                minDomainSegments: 2,
+                tlds: { allow: ["com", "net"] },
+            })
+            .lowercase()
+            .required(),
+        password: Joi.string()
+            .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+            .required(),
 
-	return userSchema.validate(data);
+    });
+
+    return userSchema.validate(data);
 };
+const categoriesValidate = (data) => {
+    const categorySchema = Joi.object({
+        cat_name: Joi.string().required(),
+    });
+    return categorySchema.validate(data);
+}
 
 module.exports = {
-	userValidate,
+    userValidate,
+    categoriesValidate,
 };
