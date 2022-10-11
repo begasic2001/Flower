0b9644ca72af4060e50ad9ec8f4c4c403f327064
sliance <sliance2402@gmail.com>
2022-10-11 23:58:08 +0700

    add CRUD subCategory

--- server/src/controller/SubCategoryController.js
+++ server/src/controller/SubCategoryController.js
@@ -1,7 +1,86 @@
-import { userValidate } from "../config/validatation";
+import { subcategoriesValidate } from "../config/validatation";
 import createError from "http-errors";
 import db from "../models/index";
-const sub_Category = async (req, res, next) => {};
-const sub_StoreCategory = async (req, res, next) => {};
-const sub_UpdateCategory = async (req, res, next) => {};
-const sub_DeleteCategory = async (req, res, next) => {};
+import { Router } from "express";
+const sub_Category = async(req, res, next) => {
+    try {
+        const subCategory = await db.Subcategories.findAll({});
+        res.json({
+            status: 1,
+            result: subCategory,
+        })
+    } catch (error) {
+        next(error);
+    }
+};
+const sub_StoreCategory = async(req, res, next) => {
+    try {
+        const { subcategory_name, categories_id } = req.body;
+        const { error } = subcategoriesValidate(req.body);
+        if (error) {
+            throw createError(error.details[0].message);
+        }
+        const isExistSubCategories = await db.Subcategories.findOne({ where: { subcategory_name, categories_id } });
+        if (isExistSubCategories) {
+            throw createError.Conflict(`${subcategory_name} or ${categories_id}is has already`);
+        }
+        const saveCreateSubCategory = await db.Subcategories.create({
+            subcategory_name,
+            categories_id,
+        });
+        res.json({
+            status: "Create Success",
+            element: saveCreateSubCategory,
+        })
+    } catch (error) {
+        next(error);
+    }
+};
+const sub_UpdateCategory = async(req, res, next) => {
+    try {
+        const id = req.params.id;
+        const { categories_id, subcategory_name } = req.body;
+        const { error } = subcategoriesValidate(req.body);
+        if (error) {
+            throw createError(error.details[0].message);
+        }
+        const isExistUpdateSubCate = await db.Subcategories.findOne({
+            where: {
+                id,
+                categories_id,
+                subcategory_name
+            }
+        });
+        if (isExistUpdateSubCate) {
+            throw createError.Conflict(`${subcategory_name} or ${categories_id} is has already `);
+        }
+        await db.Subcategories.update({
+            categories_id,
+            subcategory_name
+        }, {
+            where: { id: id }
+        });
+        res.json({
+            message: 'Update Success'
+        })
+    } catch (error) {
+        next(error);
+    }
+};
+const sub_DeleteCategory = async(req, res, next) => {
+    try {
+        const id = req.params.id;
+        await db.Subcategories.destroy({ where: { id } });
+        res.json({
+            message: "Delete Success"
+        })
+    } catch (error) {
+        next(error);
+    }
+};
+module.exports = {
+    sub_Category,
+    sub_StoreCategory,
+    sub_UpdateCategory,
+    sub_DeleteCategory,
+};
--- server/src/route/route.js
+++ server/src/route/route.js
@@ -2,11 +2,13 @@ import routerUser from "./user.js";
 import routerCategory from "./category.js";
 import routerAdmin from "./adminator.js";
 import routerBrand from "./brand.js";
+import routerSubCate from "./subcategory.js"
 const app = (app) => {
-  app.use("/api/auth", routerUser);
-  app.use("/api/cate", routerCategory);
-  app.use("/api/admin", routerAdmin);
-  app.use("/api/brand", routerBrand);
+    app.use("/api/auth", routerUser);
+    app.use("/api/cate", routerCategory);
+    app.use("/api/admin", routerAdmin);
+    app.use("/api/brand", routerBrand);
+    app.use("/api/subcate", routerSubCate);
 };
 
 module.exports = app;
