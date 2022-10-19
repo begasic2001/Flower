280a7831aa8227ea6cce6cd518997f3558c1d5c7
= <=>
2022-10-19 09:40:32 +0700

    done service brand

--- server/src/services/brand_service.js
+++ server/src/services/brand_service.js
@@ -3,6 +3,37 @@ import createError from "http-errors";
 import db from "../models/index";
 import { v4 as genarateId } from "uuid";
 const cloudinary = require("cloudinary").v2;
+
+const getAny = ({ page, limit, order, name, available, ...query }) => new Promise(async (resolve, reject) => {
+  try {
+      const queries = { raw: true, nest: true }
+      const offset = (!page || +page <= 1) ? 0 : (+page - 1)
+      const fLimit = +limit || +process.env.LIMIT_BOOK
+      queries.offset = offset * fLimit
+      queries.limit = fLimit
+      if (order) queries.order = [order]
+      if (name) query.title = { [Op.substring]: name }
+      if (available) query.available = { [Op.between]: available }
+      const response = await db.Book.findAndCountAll({
+          where: query,
+          ...queries,
+          attributes: {
+              exclude: ['category_code', 'description']
+          },
+          include: [
+              { model: db.Category, attributes: { exclude: ['createdAt', 'updatedAt'] }, as: 'categoryData' }
+          ]
+      })
+      resolve({
+          err: response ? 0 : 1,
+          mes: response ? 'Got' : 'Cannot found category',
+          bookData: response
+      })
+  } catch (error) {
+      reject(error)
+  }
+})
+
 const brand = () => {
   return new Promise(async (resolve, reject) => {
     try {
@@ -30,7 +61,7 @@ const createBrand = (data, fileData) => {
       });
       resolve({
         status: response[1] ? 0 : 1,
-        msg: response[1] ? "Created" : "Book has been created",
+        msg: response[1] ? "Created" : "Brand has been created",
       });
       if (fileData && !response[1])
         cloudinary.uploader.destroy(fileData.filename);
@@ -41,27 +72,44 @@ const createBrand = (data, fileData) => {
   });
 };
 
-const updateBrand = (data) => {
+const updateBrand = ({id,...data},fileData) => {
   return new Promise(async (resolve, reject) => {
     try {
+      if (fileData) data.image = fileData?.path
+        const response = await db.Book.update(data, {
+            where: { id }
+        })
+        resolve({
+            err: response[0] > 0 ? 0 : 1,
+            mes: response[0] > 0 ? `${response[0]} brand updated` : 'Cannot update new brand/ brand ID not found',
+        })
+        if (fileData && response[0] === 0) cloudinary.uploader.destroy(fileData.filename)
     } catch (error) {
       reject(error);
+      if (fileData) cloudinary.uploader.destroy(fileData.filename)
     }
   });
 };
 
-const deleteBrand = (data) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-    } catch (error) {
-      reject(error);
-    }
-  });
-};
+const deleteBrand = (id, filename) => new Promise(async (resolve, reject) => {
+  try {
+      const response = await db.Book.destroy({
+          where: { id }
+      })
+      resolve({
+          err: response > 0 ? 0 : 1,
+          mes: `${response} deleted`
+      })
+      cloudinary.api.delete_resources(filename)
+  } catch (error) {
+      reject(error)
+  }
+})
 
 module.exports = {
   brand,
   createBrand,
   updateBrand,
   deleteBrand,
+  getAny
 };
